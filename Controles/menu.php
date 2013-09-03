9c0925b9a493ab2bb549fbb5f6c6551e4d434503
Derik <tiger.dgp@hotmail.com>
2013-09-03 20:22:02 -0300

    Adaptação da função para gerar o menu

--- Controles/menu.php
+++ Controles/menu.php
@@ -2,40 +2,18 @@
     <form action="" name="formBusca">
     <input type="search" name="busca" placeholder="Pesquisar" autocomplete="on">
 </form>
-    <ul id="g_menu">
-        <?php
-
-            $menu_query = mysql_query("SELECT * FROM " . $prefixo_db . "menu WHERE ativo = 1 AND pai = 1");
+    
+<?php
             
-            while ($menu = mysql_fetch_assoc($menu_query)) {
-                if ($menu['submenu'] == 1) {
-                    echo "<li><span>" . $menu['nome'] . " <img src='Temas/tema1/img/down.png' alt='Down'/></span>";
-                        echo "<ul>";
-                            $submenu_query = mysql_query("SELECT * FROM " . $prefixo_db . "menu WHERE filho = " . $menu['id']);
-                            while ($menu1 = mysql_fetch_assoc($submenu_query)) {
-                                if ($menu1['pai'] == 2) {
-                                    echo "<li><span>" . $menu1['nome'] . "<img src='Temas/tema1/img/right.png' alt='Right'/></span>";
-                                        echo "<ul>";
-                                        $submenu2_query = mysql_query("SELECT * FROM " . $prefixo_db . "menu WHERE filho = " . $menu1['id']);
-                                        while ($menu2 = mysql_fetch_assoc($submenu2_query)) {
-                                            echo "<li><span>" . $menu2['nome'] . "</span></li>";
-                                        }
-                                        echo "</ul>";
-                                    echo "</li>";
-                                }
-                                else {
-                                    echo "<li><span>" . $menu1['nome'] . "</span></li>";
-                                }
-                            }
-                        echo "</ul>";
-                    echo "</li>";
-                }
-                else {
-                    echo "<li><span>" . $menu['nome'] . "</span></li>";
-                }
-            }            
+    $query = mysql_query("SELECT * FROM " . $prefixo_db . "menu ORDER BY pai");
+    while($row = mysql_fetch_object($query))
+    {
+            $menuItens[$row->pai][$row->id]= array('link'=> $row->link,'name'=> $row->nome);
+    }
+    imprimeMenuInfinito($menuItens);
 
-        ?>
+?>
+    <!--<ul>
         <li><span>Database </span>
             <ul>
                 <li><span>Items <img src="Temas/tema1/img/right.png" alt="Right"/></span>
@@ -90,5 +68,5 @@
             </ul>
         </li>
         <li><a href="#"><span>Guias</span></a></li>
-    </ul>
+    </ul>-->
 </nav>
