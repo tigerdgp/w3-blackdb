6983159e0d9a41c793c9aa3821ab79fe971c4535
tigerdgp <tiger.dgp@hotmail.com>
2013-09-03 02:43:48 -0300

    Testes da tabela para exibição dos items

--- Modulos/items.php
+++ Modulos/items.php
@@ -1,9 +1,9 @@
-d<table>
-    <tr>
-        <th>a</th>
-        <th>a</th>
-        <th>a</th>
-        <th>a</th>
+<table border="1" style="width: 100%; border: 1px solid #000;">
+    <tr  style="border: 1px solid #000;">
+        <th style="width: 10%;">Icone</th>
+        <th>Nome</th>
+        <th style="width: 14%;">Status 1</th>
+        <th style="width: 10%;">Status 2</th>
     </tr>
     <tr>
         <td>b</td>
